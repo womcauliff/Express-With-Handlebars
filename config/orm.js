51f893265cce7bd3aa9a7d83013d5f44976b5072
Will McAuliff <womcauliff@gmail.com>
2016-09-30 14:43:42 -0400

    Updates to ORM: commenting, helper functions, callback parameters

--- config/orm.js
+++ config/orm.js
@@ -1,32 +1,112 @@
 var connection = require('./connection.js');
-
+/**
+ * Handles the Object-Relational Mapping between
+ * the JS model and the DB
+ * @constructor
+ */
 var ORM = function() {
 
-	this.selectAll = function(tablename) {
-		connection.query('SELECT * FROM ??', [tablename], function(error, results, fields) {
-			if (error) throw error;
-			return results;
+	/**
+	 * Generates a string of comma delimited escape placeholder characters.
+	 *
+	 * @param {Number} num - The number of escape placeholder characters which
+	 * will be generated in the string.
+	 * @return {String} - a string of comma delimited escape placeholder
+	 * characters.
+	 */
+	function escapeValuePlaceholders(num) {
+		var arr = [];
+		for (var i = 0; i < num; i++) {
+			arr.push('?');
+		}
+		return arr.toString();
+	}
+
+	/**
+	 * Converts a JSON object of column names and values into a string needed
+	 * for an a valid SQL INSERT INTO statment.
+	 *
+	 * @param {Object} obj - a series of key-value pairs in which each key
+	 * corresponds to the column name of a particular table.
+	 * @return {String} - each key and value delimited by '='. pairs delimited
+	 * by ','
+	 */
+	function objToSql(obj) {
+		var arr = [];
+		for (var key in obj) {
+			if (obj.hasOwnProperty(key)) {
+				arr.push(key + '=' + connection.escape(obj[key]));
+			}
+		}
+		return arr.toString();
+	}
+
+	/**
+	 * Selects all columns and values from a given table.
+	 *
+	 * @param {String} tablename - a table within the database
+	 * @param {Function} callback - invoked after the query to the database.
+	 * Upon success, callback is invoked as callback(null, results), where
+	 * `results` is an array of row objects. Upon failure, callback is invoked
+	 * as callback(error) instead.
+	 */
+	this.selectAll = function(tablename, callback) {
+		var q = 'SELECT * FROM ' + tablename;
+		connection.query(q, function(error, results, fields) {
+			if (error) return callback(error);
+			callback(null, results);
 		});
 	};
 
-	this.insertOne = function(tablename, rowobject) {
-		connection.query("INSERT INTO ?? SET ??", [
-			tablename, 
-			rowobject
-		], function(error, results) {
-			if(error) throw error;
-			return results;
-		});
-	};
-
-	this.updateOne = function(tablename, rowobject, valueobject) {
-		connection.query("UPDATE ? SET ? WHERE ?", [
-			tablename,
-			rowobject,
-			valueobject
+	/**
+	 * Inserts a single row into a given table
+	 *
+	 * @param {String} tablename - a table within the database
+	 * @param {String[]} cols - columns within the given table that values will
+	 * be inserted into.
+	 * @param {String[]} vals - values which will be inserted, corresponding to
+	 * the given columns. Escaped before inserted.
+	 * @param {Function} callback - invoked after the query to the database.
+	 * Upon success, callback is invoked as callback(null, results), where
+	 * `results` is an array of row objects. Upon failure, callback is invoked
+	 * as callback(error) instead.
+	 */
+	this.insertOne = function(tablename, cols, vals, callback) {
+		var q = 'INSERT INTO ' + tablename
+				+ '('
+				+ cols.toString()
+				+ ')'
+				+ 'VALUES ('
+				+ escapeValuePlaceholders(vals.length)
+				+ ')';
+		connection.query(q, [
+			vals
 		], function(error, results, fields) {
-			if (error) throw error;
-			return results;
+			if(error) return callback(error);
+			callback(null, results);
+		});
+	};
+
+	/**
+	 * Updates the columns of a single row in a given table
+	 *
+	 * @param {String} tablename - a table within the database.
+	 * @param {Object} columnValueObj - a series of key-value pairs in which
+	 * each key corresponds to the column name of a particular table. Values
+	 * will be escaped before updating.
+	 * @param {String} condition - used within a `WHERE` clause.
+	 * @param {Function} callback - invoked after the query to the database.
+	 * Upon success, callback is invoked as callback(null, results), where
+	 * `results` is an array of row objects. Upon failure, callback is invoked
+	 * as callback(error) instead.
+	 */
+	this.updateOne = function(tablename, columnValueObj, condition, callback) {
+		var q = 'UPDATE ' + tablename
+				+ 'SET ' + objToSql(columnValueObj)
+				+ 'WHERE ' + connection.escape(condition);
+		connection.query(q, function(error, results, fields) {
+			if(error) return callback(error);
+			callback(null, results);
 		});
 	};
 }
